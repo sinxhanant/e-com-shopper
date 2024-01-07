0290e2a4c17259f074e9430bbb9e8a565cff6d47
Anant Prakash Singh <95313442+sinxhanant@users.noreply.github.com>
2024-01-07 14:52:53 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Review (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Are you attending a special occasion and want to look your best? If so, you’ll look great in one of our suits. As far as elegance and class go, they are unparalleled. Our suits are fashionable, sparkling, lightweight, and easy to wear.</p>
+        <p>Dresses featuring a sweetheart neckline, rhinestones, a wrap, a midi length, or an evening’s worth of sequins are all available. They are offered in a rainbow of shades, allowing you to dress to impress for your ever-changing life occasions.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
